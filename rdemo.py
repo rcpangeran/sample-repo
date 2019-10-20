754243c9bddcbd533f44a271b0b247e91b213f26
Rio C. Pangeran <rio.pangeran@gmail.com>
2019-10-20 18:19:47 +0700

    Removed some code

--- rdemo.py
+++ rdemo.py
@@ -1,17 +1,5 @@
-import math
-from os import rename
-
 import requests
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-name = input("Your name?")
-print("Hello, ", name)
-
+print(r.ok)
